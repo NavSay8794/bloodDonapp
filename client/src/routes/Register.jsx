f44ef0b907b990fd2e45ea4541bbf7aa4a00c80c
Naveed Sayyed <sayyednaveed107@gmail.com>
2025-11-06 21:40:50 +0530

    blood don app 2

--- client/src/routes/Register.jsx
+++ client/src/routes/Register.jsx
@@ -1,17 +1,25 @@
-import React , { useState } from 'react'
+import React, { useState } from "react";
+import { register } from "../utils/axiosutil";
 
 const Register = () => {
-    const [fullName, setFullName] = useState('')
-    const [email, setEmail] = useState('')
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
+  const handleRegisterSubmit = async (e) => {
+    e.preventDefault();
 
-  const handleRegisterSubmit = ()=>{
-    e.preventDefault()
-
-  }
+    const user = {
+      fullName: fullName,
+      email: email,
+      username: username,
+      password: password,
+    };
+    const resp = await register(user);
+    navigate("/");
+  };
   return (
     <div>
       <h2>Register</h2>
@@ -19,38 +27,38 @@ const Register = () => {
         <input
           type="text"
           value={fullName}
-          placeholder='Full Name'
+          placeholder="Full Name"
           onChange={(e) => setFullName(e.target.value)}
         />
         <input
           type="text"
           value={email}
-          placeholder='Email'
+          placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="text"
           value={username}
-          placeholder='Username'
+          placeholder="Username"
           onChange={(e) => setUsername(e.target.value)}
         />
         <input
           type="password"
           value={password}
-          placeholder='Password'
+          placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
         />
         <input
           type="password"
           value={confirmPassword}
-          placeholder='Confirm Password'
+          placeholder="Confirm Password"
           onChange={(e) => setConfirmPassword(e.target.value)}
         />
 
         <button type="submit">Register</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
