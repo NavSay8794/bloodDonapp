9e21c6f6c84298a9f9c4f8ab95b423129610c5ec
Naveed Sayyed <sayyednaveed107@gmail.com>
2025-11-06 18:38:45 +0530

    blood don app 2

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -7,70 +7,75 @@ const Home = () => {
 
   const getAllRequestsData = async () => {
     const resp = await getAllRequests();
-    setOpenReq(resp.data.reqsData)
+    setOpenReq(resp.data.reqsData);
   };
 
-
   useEffect(() => {
-    getAllRequestsData()
-  },[]);
-
+    getAllRequestsData();
+  }, []);
 
   return (
     <div>
       <h1>Requests</h1>
-      <div style={{ overflowX: "auto" }}>
-        <table
-          className="requests-table"
-          style={{
-            width: "100%",
-            borderCollapse: "collapse",
-            border: "1px solid #ddd",
-          }}
-        >
-          <thead>
-            <tr>
-              <th
-                style={{
-                  border: "1px solid #ddd",
-                  padding: "8px",
-                  textAlign: "left",
-                }}
-              >
-                Title
-              </th>
-              <th
-                style={{
-                  border: "1px solid #ddd",
-                  padding: "8px",
-                  textAlign: "left",
-                }}
-              >
-                Status
-              </th>
-              <th
-                style={{
-                  border: "1px solid #ddd",
-                  padding: "8px",
-                  textAlign: "left",
-                }}
-              >
-                View Request Details
-              </th>
-              
-            </tr>
-          </thead>
-          <tbody>
-            {openReq.map((item) => {
-              return <tr>
-                <td>{item.title}</td>
-                <td>{item.status}</td>
-                <td><Link to={`/bloodRequest/${item._id}`}>View</Link></td>
+      {openReq.length === 0 ? (
+        <h1>Welcome to Blood Donation Request App</h1>
+      ) : (
+        <div style={{ overflowX: "auto" }}>
+          <table
+            className="requests-table"
+            style={{
+              width: "100%",
+              borderCollapse: "collapse",
+              border: "1px solid #ddd",
+            }}
+          >
+            <thead>
+              <tr>
+                <th
+                  style={{
+                    border: "1px solid #ddd",
+                    padding: "8px",
+                    textAlign: "left",
+                  }}
+                >
+                  Title
+                </th>
+                <th
+                  style={{
+                    border: "1px solid #ddd",
+                    padding: "8px",
+                    textAlign: "left",
+                  }}
+                >
+                  Status
+                </th>
+                <th
+                  style={{
+                    border: "1px solid #ddd",
+                    padding: "8px",
+                    textAlign: "left",
+                  }}
+                >
+                  View Request Details
+                </th>
               </tr>
-            })}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {openReq.map((item) => {
+                return (
+                  <tr>
+                    <td>{item.title}</td>
+                    <td>{item.status}</td>
+                    <td>
+                      <Link to={`/bloodRequest/${item._id}`}>View</Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
